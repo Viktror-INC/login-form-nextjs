1e300a8ab9dc000905af9503d3c94c6c7a15b1ea
Vik <v.tesliuk@carma.taxi>
2024-06-10 10:57:38 +0300

    fix: rename component to project style

--- src/pages-views/main-page-view/index.tsx
+++ src/pages-views/main-page-view/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import MainWrapper from "src/shared/components/MainWrapper";
+import MainWrapper from "src/shared/components/main-wrapper";
 import LoginForm from "./components/login-form";
 
 const MainPageView = () => {
